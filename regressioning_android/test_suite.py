1a7d69ca6679e462faf7239f702a8ea8c96ec102
yeonshin.kang@myrealtrip.com <>
2018-10-24 17:37:28 +0900

    첫번째 커밋

---
+++ regressioning_android/test_suite.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+
+import unittest
+import pytest
+
+# from regressioning_android.android_01_RT import RegressionOne
+# from regressioning_android.android_03_RT import RegressionTwo
